f060029dd49df0bbb167c92331ed35511ffd14e9
sween <khansween@gmail.com>
2023-07-05 23:13:54 +0530

    created home file in front

---
+++ front/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Register from './FORM/Register'
+import Login from './FORM/Login'
+
+function Home() {
+  return (
+    <div>
+        <Login />
+        <Register />
+    </div>
+  )
+}
+
+export default Home
