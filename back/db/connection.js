e0fdff1bc879c58b68a52d771574c8d6828b2597
sween <khansween@gmail.com>
2023-07-17 23:55:08 +0530

    made some changes in book component

--- back/db/connection.js
+++ back/db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // const connection = mongoose.connect("mongodb+srv://Rebel_khan:<password>@cluster0.iooq12u.mongodb.net/?retryWrites=true&w=majority")
-const connection = mongoose.connect("mongodb+srv://Rebel_khan:Rebel!1997@cluster0.iooq12u.mongodb.net/?retryWrites=true&w=majority")
+const connection = mongoose.connect("mongodb+srv://SweenKhan:Sween!1997@cluster0.zimszkh.mongodb.net/?retryWrites=true&w=majority")
 
 
 export default connection;                    
