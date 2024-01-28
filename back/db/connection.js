f422db5da27f9e996d23c8e6e7078c228ef7b343
Sween Khan <136142278+Sweenkhan@users.noreply.github.com>
2024-01-28 23:36:34 +0530

    Update connection.js

--- back/db/connection.js
+++ back/db/connection.js
@@ -2,6 +2,4 @@ import mongoose from "mongoose";
 
  const connection = mongoose.connect("mongodb+srv://Rebel_khan:Rebel!1997@cluster0.iooq12u.mongodb.net/?retryWrites=true&w=majority")
 
-
-
 export default connection;                    
