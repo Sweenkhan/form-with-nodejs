be8995e1445d8115709203d9ceec886ddb4893cc
Sween Khan <136142278+Sweenkhan@users.noreply.github.com>
2024-04-29 23:49:18 +0530

    Update index.js

--- back/index.js
+++ back/index.js
@@ -8,6 +8,7 @@ import bcrypt from "bcrypt";
 const app = express(); 
 
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
